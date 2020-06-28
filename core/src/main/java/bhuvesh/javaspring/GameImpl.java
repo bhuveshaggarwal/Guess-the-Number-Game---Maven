59550b13a39e0415d475c0237bff32ac66268921
bhuveshaggarwal <bhuveshaggarwal@gmail.com>
2020-06-28 18:08:59 +0530

    Guess the Number Game - Autowiring Beans.

--- core/src/main/java/bhuvesh/javaspring/GameImpl.java
+++ core/src/main/java/bhuvesh/javaspring/GameImpl.java
@@ -2,6 +2,7 @@ package bhuvesh.javaspring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,6 +13,7 @@ public class GameImpl implements Game {
 
     // == fields ==
 
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -44,11 +46,10 @@ public class GameImpl implements Game {
     }
 
     // == public methods ==
-
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
+//
+//    public void setNumberGenerator(NumberGenerator numberGenerator) {
+//        this.numberGenerator = numberGenerator;
+//    }
 
     @Override
     public int getNumber() {
