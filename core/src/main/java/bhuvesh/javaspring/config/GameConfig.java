00b0d7488bf7d7abe3e08798cf208eee24f7c871
bhuveshaggarwal <bhuveshaggarwal@gmail.com>
2020-07-03 22:41:16 +0530

    Guess the Number Game - Using Properties.

--- core/src/main/java/bhuvesh/javaspring/config/GameConfig.java
+++ core/src/main/java/bhuvesh/javaspring/config/GameConfig.java
@@ -2,15 +2,21 @@ package bhuvesh.javaspring.config;
 
 import bhuvesh.javaspring.GuessCount;
 import bhuvesh.javaspring.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
-    private int guessCount = 8;
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+
+    @Value("${game.guessCount}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
